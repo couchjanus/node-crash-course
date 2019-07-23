90471b53a38240571778b12bff404d4bbdde1631
couch Janus <couchjanus@gmail.com>
2019-07-23 16:50:47 +0300

    added unit 1

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const http = require('http');
-const port = process.env.PORT || 3000
+const port = 3000;
+const hostname = 'localhost';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
+// const server = http.createServer((req, res) => {
+//   // the same kind of magic happens here!
+//   res.statusCode = 200;
+//   res.statusCode = 404; // Сообщаем клиенту, что ресурс не найден.
+//   res.setHeader('Content-Type', 'application/json');
+//   res.setHeader('X-Powered-By', 'bla bla bla');
+//   res.end();
+// }).listen(port);
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     // 'Content-Type': 'application/json',
+//     'Content-Type': 'text/plain',
+//     'X-Powered-By': 'bla bla bla'
+//   });
+
+//   res.write('<html>');
+//   res.write('<body>');
+//   res.write('<h1>Hello, World!</h1>');
+//   res.write('</body>');
+//   res.write('</html>');
+//   res.end();
+// }).listen(port);
+
+// const server = http.createServer((req, res) => {
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<html><body><h1>Hello, World!</h1></body></html>');
+// }).listen(port);
+
+// const server = http.createServer((req, res) => {
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<html><body><h1>Hello, World!</h1></body></html>');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
